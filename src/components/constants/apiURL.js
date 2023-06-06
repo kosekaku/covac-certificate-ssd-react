1c52bbce6590acfeb0840396240ed32e9aaa8b8c
Kose <fft4545@gmail.com>
2023-06-07 00:04:44 +0200

    Update apiURL.js
    Update api URLs with Glitch production deployment

--- src/components/constants/apiURL.js
+++ src/components/constants/apiURL.js
@@ -1,15 +1,15 @@
-const teiURL = 'http://localhost:8000/api/v1/teis';
-const postPrintsURL = 'http://localhost:8000/api/v1/teis/prints';
+const teiURL = 'https://api-covac-ssd.glitch.me/api/v1/teis';
+const postPrintsURL = 'https://api-covac-ssd.glitch.me/api/v1/teis/prints';
 const verifyCertURLAPI = (id) =>
-  `http://localhost:8000/api/v1/teis/verify/${id}`;
+  `https://api-covac-ssd.glitch.me/api/v1/teis/verify/${id}`;
 
 //QRCODE
 const verifyCertURL = (urlOrgin, teiId) =>
   `${urlOrgin}/print/verify?id=${teiId}`;
 
 // monitor
-const printsURL = 'http://localhost:8000/api/v1/teis/prints';
-const loginURL = 'http://localhost:8000/api/v1/login';
+const printsURL = 'https://api-covac-ssd.glitch.me/api/v1/teis/prints';
+const loginURL = 'https://api-covac-ssd.glitch.me/api/v1/login';
 
 export {
   teiURL,
